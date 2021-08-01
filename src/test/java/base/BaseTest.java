82efcb58a494764b3964015a734b3cec7a5bff3e
Joaquin Grassi <joaquin.grassi92@gmail.com>
2021-08-01 13:29:57 -0300

    eliminacion de iframe cargado en cada nueva pagina

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -2,10 +2,16 @@ package base;
 
 import Pages.HomePage;
 import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import runner.browser_manager.DriverManager;
 import runner.browser_manager.DriverManagerFactory;
 import runner.browser_manager.DriverType;
+
+import java.time.Duration;
 
 
 /**
@@ -25,8 +31,9 @@ public class BaseTest {
         driverManager= DriverManagerFactory.getManager(DriverType.CHROME);
         driver=driverManager.getDriver();
         driver.get("https://demoqa.com/");
-        //driver.manage().window().maximize();
+        driver.manage().window().maximize();
         homePage=new HomePage(driver);
+        driver.findElement(By.xpath("//*[@id=\"close-fixedban\"]")).click();
     }
 
    @AfterEach
